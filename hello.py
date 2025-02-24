dfa3ac97980b10cb3c02d2971994d97eb2476c6e
Alrich Strauss <alrich.strauss@mmltd.co.za>
2025-02-24 17:18:27 +0200

    Final changes to hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 def greet_user(name):\n    print(f"Hello, {name}!")\n\ndef main():\n    user_name = input("Enter your name: ")\n    greet_user(user_name)\n\nif __name__ == "__main__":\n    main()
+def say_hello():
+
+def greet_user(name):
+
+def farewell_user(name):
+
+def main():
+    user_name = input("Enter your name: ")
+    greet_user(user_name)
+    farewell_user(user_name)
+
+if __name__ == "__main__":
+    say_hello()
+    main()
